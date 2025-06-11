120f6ba122898068eec2b8a38279528190183118
tanuj1993 <grajni235@gmail.com>
2025-06-11 16:10:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!. Tanuj this side. I am a devops man. I am good boy.");
+        System.out.println("Hello, World!. Tanuj this side. I am a devops man. I am good boy1.");
     }
 }
